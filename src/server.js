93b5aa51a12df566cef0c8b19a46c65b6023533f
Extern Labs <externlabs@Externs-MacBook-Air.local>
2023-10-30 10:45:04 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Simulated state of the light (initially off)
+let isLightOn = false;
+
+// GET method to check the light status
+app.get('/light', (req, res) => {
+    res.json({ lightStatus: isLightOn ? 'on' : 'off' });
+});
+
+// PATCH method to toggle the light status
+app.patch('/light', (req, res) => {
+    isLightOn = !isLightOn;
+    res.json({ lightStatus: isLightOn ? 'on' : 'off' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
