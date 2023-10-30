f59a2152ac9a46e96bb0867ce2e5718d5cf44fba
Extern Labs <externlabs@Externs-MacBook-Air.local>
2023-10-30 11:20:40 +0530

    cors add

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var cors = require('cors')
 const app = express();
 const port = 3000;
+
+app.use(cors())
 
 app.use(bodyParser.json());
 
